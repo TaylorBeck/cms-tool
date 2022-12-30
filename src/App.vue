8b786202ade24d6a9323862c90ae60673b78e89d
Taylor Beck <beck.taylorg@gmail.com>
2022-12-30 12:16:27 -0800

    Built initial homepage using side navigation

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <SideLayout>
+    <Summary />
+  </SideLayout>
+</template>
+
+<script setup>
+import SideLayout from './components/SideLayout.vue';
+import Summary from './components/Summary.vue';
+import Settings from './components/Settings.vue';
+import SignIn from './components/SignIn.vue';
+</script>
+
+<style>
+</style>
